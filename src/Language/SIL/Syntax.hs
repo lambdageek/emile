3dcbbb2ceebbdf6a3158f877d222a92186578e31
Aleksey Kliger <akliger@gmail.com>
2015-11-10 20:30:34 -0500

    Some SIL syntax cleanup

--- src/Language/SIL/Syntax.hs
+++ src/Language/SIL/Syntax.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE PolyKinds, TypeFamilies, GADTs, UndecidableInstances, ConstraintKinds #-}
 module Language.SIL.Syntax where
 
+import qualified Data.Coerce
+
 import Names
 
 import Language.CoreLang
@@ -13,7 +15,7 @@ class CoreLang lang => CoreSIL lang where
 
 
 newtype Label = Label String
-              deriving (Show, Generic)
+              deriving (Show, Eq, Ord, Generic)
 
 data Σ lang =
   ValΣ (CoreType lang)
@@ -25,7 +27,7 @@ data Σ lang =
     deriving (Show, Generic)
 
 newtype Ξ lang =
-  Ξ (Bind [(TyVar lang, Embed (CoreKind lang) )] (Σ lang))
+  Ξ (Bind (TyVarBinds lang) (Σ lang))
   deriving (Show, Generic)
 
 type TyVar lang = Name (CoreType lang)
@@ -36,16 +38,33 @@ type TyVarBinds lang = [(TyVar lang, Embed (CoreKind lang))]
 type IdM lang = Name (Mod lang)
 
 data Mod lang =
+  -- X
   VarM (IdM lang)
+  -- [e]
   | ValM (CoreExpr lang)
+  -- [τ:κ]
   | TyM (CoreType lang) (CoreKind lang)
+  -- [Ξ]
   | SigM (Ξ lang)
+  -- {⋯, ℓ = M, ⋯}
   | RecordM [(Label, (Mod lang))]
+  -- M.ℓ₁.ℓ₂…
   | ProjM (Mod lang) [Label]
     -- ∀ αs:κs . λ X : Σ . pack ⟨τs, M⟩ as ∃βs:κ′s.Σ′
   | LamM (Bind (Rebind (TyVarBinds lang) ((IdM lang), Embed (Σ lang))) (PackMod lang))
   | AppM (Mod lang) [CoreType lang] (Mod lang)
-  | LetM (Bind ([TyVar lang], IdM lang, Embed (Mod lang)) (Mod lang))
+    -- unpack ⟨αs, X⟩ = M in M'
+  | UnpackM (Bind ([TyVar lang], IdM lang, Embed (Mod lang)) (Mod lang))
+    -- pack ⟨τs, M'⟩ as ∃βs:κ's.Σ'
+  | PackM (PackMod lang)
+    -- f M
+  | CoerM (SubsigCoercion lang) (Mod lang)
+  deriving (Show, Generic)
+
+data SubsigCoercion lang =
+  -- the identity coercion at Σ
+  IdCo (Σ lang)
+  -- TODO: more here
   deriving (Show, Generic)
 
 data PackMod lang =
@@ -53,13 +72,75 @@ data PackMod lang =
   deriving (Show, Generic)
 
 
-type TypeableCore lang = (Typeable lang, Typeable (CoreKind lang), Typeable (CoreType lang))
-type AlphaCore lang = (Alpha (CoreKind lang), Alpha (CoreType lang))
-
 instance Alpha Label
-instance (CoreLang lang, TypeableCore lang, AlphaCore lang) => Alpha (Σ lang)
-instance (CoreLang lang, TypeableCore lang, AlphaCore lang) => Alpha (Ξ lang)
+instance (CoreLang lang, Typeable (CoreKind lang), Typeable (CoreType lang),
+          Alpha (CoreKind lang), Alpha (CoreType lang))
+         => Alpha (Σ lang)
+instance (CoreLang lang, Typeable (CoreKind lang), Typeable (CoreType lang),
+          Alpha (CoreKind lang), Alpha (CoreType lang))
+         => Alpha (Ξ lang)
 
-instance (CoreLang lang, TypeableCore lang, AlphaCore lang, Typeable (Mod lang), Alpha (CoreExpr lang)) => Alpha (Mod lang)
-instance (CoreLang lang, TypeableCore lang, AlphaCore lang, Typeable (Mod lang), Alpha (CoreExpr lang)) => Alpha (PackMod lang)
+instance (CoreLang lang, Typeable (CoreKind lang), Typeable (CoreType lang),
+          Typeable (Mod lang),
+          Alpha (CoreKind lang), Alpha (CoreType lang), Alpha (CoreExpr lang))
+         => Alpha (Mod lang)
+instance (CoreLang lang, Typeable (CoreKind lang), Typeable (CoreType lang),
+          Typeable (Mod lang),
+          Alpha (CoreKind lang), Alpha (CoreType lang), Alpha (CoreExpr lang))
+         => Alpha (PackMod lang)
+instance (CoreLang lang, Typeable (CoreKind lang), Typeable (CoreType lang),
+          Typeable (Mod lang),
+          Alpha (CoreKind lang), Alpha (CoreType lang), Alpha (CoreExpr lang))
+         => Alpha (SubsigCoercion lang)
+
+
+instance (CoreLang lang,
+          Alpha (CoreKind lang), Alpha (CoreType lang),
+          Subst (CoreType lang) (CoreType lang),
+          Subst (CoreType lang) (CoreKind lang),
+          Generic (CoreType lang),
+          Typeable (CoreKind lang), Typeable (CoreType lang)
+         )
+         => Subst (CoreType lang) (Ξ lang)
+
+instance (CoreLang lang,
+          Alpha (CoreKind lang), Alpha (CoreType lang),
+          Subst (CoreType lang) (CoreType lang),
+          Subst (CoreType lang) (CoreKind lang),
+          Generic (CoreType lang),
+          Typeable (CoreKind lang), Typeable (CoreType lang)
+         )
+         => Subst (CoreType lang) (Σ lang)
+
+instance Subst (CoreType lang) Label
+
+mkΞ :: (CoreLang lang, Typeable (CoreKind lang), Typeable (CoreType lang),
+        Alpha (CoreKind lang), Alpha (CoreType lang), Alpha (CoreExpr lang))
+       => [(TyVar lang, CoreKind lang)]
+       -> Σ lang
+       -> Ξ lang
+mkΞ ακs = Ξ . bind (embedMap ακs)
+  where
+    embedMap :: [(a, b)] -> [(a, Embed b)]
+    embedMap = Data.Coerce.coerce
+
+unΞ :: (CoreLang lang, Typeable (CoreKind lang), Typeable (CoreType lang),
+        Alpha (CoreKind lang), Alpha (CoreType lang), Alpha (CoreExpr lang),
+        Fresh m)
+       => Ξ lang
+       -> m ([(TyVar lang, CoreKind lang)], Σ lang)
+unΞ (Ξ bnd) = do
+  (ακs, σ) <- unbind bnd
+  return (unembedMap ακs, σ)
+  where
+    unembedMap :: [(a, Embed b)] -> [(a, b)]
+    unembedMap = Data.Coerce.coerce
+
+unpackM :: (CoreLang lang, Typeable (CoreKind lang), Typeable (CoreType lang), Typeable (Mod lang),
+            Alpha (CoreKind lang), Alpha (CoreType lang), Alpha (CoreExpr lang))
+           => [(TyVar lang)] -> IdM lang -> Mod lang -> Mod lang -> Mod lang
+unpackM αs x m1 = UnpackM . bind (αs, x, embed m1)
+
+packM :: [CoreType lang] -> (Mod lang) -> (Ξ lang) -> Mod lang
+packM τs m = PackM . PackMod τs m
 
